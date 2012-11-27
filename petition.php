2e472b6862f8d2eb866fd7572efd31755f14f931
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2012-11-27 07:19:42 +0100

    initial commit.

---
+++ petition.php
@@ -0,0 +1,10 @@
+<?php
+	$url = $_GET['url'];
+    $petition='http://elchudi.xen.prgmr.com:8000/' . $url;
+    if($url!="")
+		echo header('Content-Type: application/json');
+		echo json_encode(utf8_encode(str_replace("'", '"', file_get_contents($petition))));
+		//echo utf8_encode(str_replace("'", '"', file_get_contents($petition)));
+		//echo json_encode(utf8_encode(('{"one": "Singular sensation","two": "Beady little eyes","three": "Little birds pitch by my doorstep"}')));
+		//echo str_replace("'", '"', file_get_contents($url));
+?>
